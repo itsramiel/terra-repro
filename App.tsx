35062afa92079ecbbf6ca7981a523afeb2182190
ramiel <ramielwan48@gmail.com>
2023-05-25 08:49:47 +0300

    reproduce apple health userId bug

--- App.tsx
+++ App.tsx
@@ -1,49 +1,111 @@
 import * as Terra from 'terra-react';
-import {useEffect} from 'react';
-import {Button, View} from 'react-native';
+import {Alert, Button, View} from 'react-native';
 import Config from 'react-native-config';
 
 const DEV_ID = Config.DEV_ID;
 const API_KEY = Config.API_KEY;
 
-const REF_ID = 'dnejfekfne';
-
-const FROM_DATE = new Date();
-FROM_DATE.setDate(FROM_DATE.getDate() - 1);
-const TO_DATE = new Date();
+const REF_ID = 'dnejfekfnecneo';
 
 function App() {
-  useEffect(() => {
-    Terra.initTerra(DEV_ID, REF_ID).then(async args => {
-      console.log(args);
-      const res = await fetch(
-        'https://api.tryterra.co/v2/auth/generateAuthToken',
-        {
-          method: 'POST',
-          headers: {
-            'dev-id': DEV_ID,
-            'x-api-key': API_KEY,
-          },
-        },
-      );
-      const token = (await res.json()).token;
-      Terra.initConnection(Terra.Connections.APPLE_HEALTH, token, true);
-    });
-  }, []);
+  const initTerra = () => {
+    Terra.initTerra(DEV_ID, REF_ID).then(() =>
+      Alert.alert('Terra initialized'),
+    );
+  };
 
-  const getBodyData = () => {
-    Terra.getBody(Terra.Connections.APPLE_HEALTH, FROM_DATE, TO_DATE, false)
-      .then(result => {
-        console.log(result);
-      })
-      .catch(e => {
-        console.log(e);
-      });
+  const initApple = async () => {
+    const res = await fetch(
+      'https://api.tryterra.co/v2/auth/generateAuthToken',
+      {
+        method: 'POST',
+        headers: {
+          'dev-id': DEV_ID,
+          'x-api-key': API_KEY,
+        },
+      },
+    );
+    const token = (await res.json()).token;
+    const {success} = await Terra.initConnection(
+      Terra.Connections.APPLE_HEALTH,
+      token,
+      true,
+    );
+    Alert.alert(`Apple health initialized with success value: ${success}`);
+  };
+
+  const checkAppleLocal = async () => {
+    const {userId} = await Terra.getUserId(Terra.Connections.APPLE_HEALTH);
+    Alert.alert(`User has apple health connection of userId: ${userId}`);
+  };
+  const checkAppleRemote = async () => {
+    const {userId} = await Terra.getUserId(Terra.Connections.APPLE_HEALTH);
+    var myHeaders = new Headers();
+    myHeaders.append('dev-id', DEV_ID);
+    myHeaders.append('X-API-Key', API_KEY);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+
+    const response = await (
+      await fetch(
+        `https://api.tryterra.co/v2/userInfo?user_id=${userId}`,
+        requestOptions,
+      )
+    ).json();
+
+    Alert.alert(
+      `Remote checking of apple health for userId ${userId} ise ${JSON.stringify(
+        response,
+      )}`,
+    );
+  };
+
+  const deauthorizeAppleHealth = async () => {
+    const {userId} = await Terra.getUserId(Terra.Connections.APPLE_HEALTH);
+    if (userId === null) {
+      Alert.alert('Cant deauthorize since userId is null');
+      return;
+    }
+    var myHeaders = new Headers();
+    myHeaders.append('dev-id', DEV_ID);
+    myHeaders.append('X-API-Key', API_KEY);
+
+    var requestOptions = {
+      method: 'DELETE',
+      headers: myHeaders,
+    };
+
+    const response = await (
+      await fetch(
+        `https://api.tryterra.co/v2/auth/deauthenticateUser?user_id=${userId}`,
+        requestOptions,
+      )
+    ).json();
+
+    Alert.alert(
+      `deauthorization response of Apple Health: ${JSON.stringify(response)}`,
+    );
   };
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button title="get body data" onPress={getBodyData} />
+      <Button title="init terra" onPress={initTerra} />
+      <Button title="init apple health" onPress={initApple} />
+      <Button
+        title="check apple health connection locally"
+        onPress={checkAppleLocal}
+      />
+      <Button
+        title="check apple health connection from api"
+        onPress={checkAppleRemote}
+      />
+      <Button
+        title="deauthorize apple health"
+        onPress={deauthorizeAppleHealth}
+      />
     </View>
   );
 }
